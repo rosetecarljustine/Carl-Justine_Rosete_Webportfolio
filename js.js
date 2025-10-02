24655be461fccf8bebcd5cd228c18d7a33974730
rosetecarljustine <rosetecarljustine32@gmail.com>
2025-10-02 13:09:46 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function goToSection(sectionId) {
+    const section =
+    document.getElementById(sectionId);
+    
+    if (section) {
+        section.scrollIntoView({behavior:'smooth'});
+    } 
+}
